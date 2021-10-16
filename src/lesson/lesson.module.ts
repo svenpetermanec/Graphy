31cc5e68e000231ea6d3825f14461076a950cae6
Sven Petermanec <epsteve@ericsson.com>
2021-10-16 09:19:04 +0200

    Implement lesson module, resolver, service and entity

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Lesson])],
+  providers: [LessonResolver, LessonService],
+})
 export class LessonModule {}
